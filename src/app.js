c4ba8ec73eae4ec3815fdf342bfa670f1a5a94ff
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2024-11-18 11:50:44 +0100

    Added db connection

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { config } from 'dotenv';
+import { dbConnection } from './db_config/database.js';
 
 config();
 
@@ -8,8 +9,11 @@ app.use(express.json());
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-    console.log(`Servern är igång på ${PORT}`);
+app.listen(PORT, async () => {
+    const connection = await dbConnection()
+
+    
+    console.log(`Servern är igång på PORT ${PORT} ....`);
     
 })
 
