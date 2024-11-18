ada6ef056edac11b1bdff1f8e8fc51d2cb1ad230
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2024-11-18 10:54:33 +0100

    I created an npm init package.json, installed express, mysql2, node-cron, dotenv, and nodemon.

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Servern är igång på ${PORT}`);
+    
+})
+
+
