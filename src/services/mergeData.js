5e73805c075b416221bff3234dbcd364e63f2304
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2024-12-04 09:15:09 +0100

    Fixed giftcards sent

--- src/services/mergeData.js
+++ src/services/mergeData.js
@@ -16,15 +16,20 @@ export default async function mergeData() {
 
       const mergedData = {};
 
+      const normalizePhone = (phone) => {
+      if (!phone) return;
+
+      if (phone.slice(0, 1) === "0") {
+        phone = "46" + phone.slice(1);
+      } else if (phone.slice(0, 3) === "+46") {
+        phone = "46" + phone.slice(3);
+      }
+      return phone; // Om redan i korrekt format, behåll det
+    }
+
       // Process participant data as before...
       participants.forEach((participant) => {
-        let phone = participant.telephone;
-
-        if (phone.slice(0, 1) === "0") {
-          phone = "46" + phone.slice(1);
-        } else if (phone.slice(0, 3) === "+46") {
-          phone = "46" + phone.slice(3);
-        }
+        let phone =  normalizePhone(participant.telephone);
 
         if (!mergedData[phone]) {
           mergedData[phone] = {
@@ -52,6 +57,8 @@ export default async function mergeData() {
             latest_date: participant.modified,
             paidCounter: 0,
             giftCounter: 0,
+            created: "",
+            modified: "",
             phone,
           };
         }
@@ -60,55 +67,66 @@ export default async function mergeData() {
         if (participant.location) data.locations.add(participant.location);
         data.participants_id.add(participant.id);
         data.total_amount += +participant.amount;
-        if (participant.coupon_sent) data.giftcards_sent++;
+        data.giftcards_sent += +participant.coupon_sent;
         data.quiz_answers += parseFloat(participant.points_scored) || 0;
         data.custom_field_1 += parseFloat(participant.time_spent) || 0;
         data.custom_field_2 += parseInt(participant.sms_parts) || 0;
         data.custom_field_3 += parseFloat(participant.sms_cost) || 0;
         data.custom_field_5 += 1;
-        data.affiliated_leads_generated += participant.receiver_phone ? 1 : 0;
+        data.affiliated_leads_generated += participant.telephone_receiver_phone ? 1 : 0;
         data.affiliated_money_generated += parseFloat(participant.amount) || 0;
 
         // Update other properties if necessary
-        if (participant.name) data.name = participant.name;
-        if (participant.email) data.email = participant.email;
-        if (participant.address) data.address = participant.address;
-        if (participant.postcode) data.zip = participant.postcode;
-        if (participant.city) data.city = participant.city;
-        if (participant.personal_number)
-          data.personal_number = participant.personal_number;
-        if (participant.modified > data.latest_date)
-          data.latest_date = participant.modified;
+        data.name = participant.name;
+        data.email = participant.email;
+        data.address = participant.address;
+        data.zip = participant.postcode;
+        data.city = participant.city;
+        data.personal_number = participant.personal_number;
+        data.created = new Date().toISOString().slice(0, 19).replace('T', ' ');
+        data.modified = new Date().toISOString().slice(0, 19).replace('T', ' ');
+        data.latest_date = participant.modified;
         
-        if (participant.recurring_history === '14') {
-          data.paidCounter++;  
+        const paidCount = participants.filter(
+          (p) => p.recurring_history === "14" && normalizePhone(p.telephone) === phone
+        ).length;
+        
+        if (paidCount > 0) {
+          const text = `Paid x${paidCount}`;
+          if (!data.all_dates.includes(text)) {
+            data.all_dates.push(text);
+          }
         }
-      
+
+        const giftCount = participants.filter((p) => p.coupon_sent && 
+        p.telephone_receiver_phone !== null &&
+        p.telephone_receiver_phone !== "" &&
+        normalizePhone(p.telephone) === phone).length;
+
+        if(giftCount > 0){
+          const textGift = `Giftcards Sent ${giftCount}`;
+          if (!data.all_dates.includes(textGift)) {
+            data.all_dates.push(textGift);
+          }
+        }
+
         if (participant.recurring_history === '6') {
           if (!data.all_dates.includes('Petition')) {
             data.all_dates.push('Petition'); 
           }
         }
-
-        if(participant.coupon_sent && 
-          participant.telephone_receiver_phone !== null && 
-          participant.telephone_receiver_phone !== ""){
-
-          data.giftCounter++;
-        }
-
+        
         if(participant.agree_download_report === 0){
             if(!data.all_dates.includes('No newsletter')){
-              data.all_dates.push('No newslatter')
+              data.all_dates.push('No newsletter')
             }
         }
 
         if (participant.recurring_history === "4" && participant.game_type) {
           if (!data.tags.includes(participant.game_type)) {
-            data.tags = participant.game_type;
+            data.all_dates.push(participant.game_type);
           }
         }
-        
       });
 
       // Insert or update the leads table
@@ -116,19 +134,7 @@ export default async function mergeData() {
         const data = mergedData[phone];
         data.locations = Array.from(data.locations).join(", ");
         data.participants_id = Array.from(data.participants_id).join(", ");
-
-        if (data.paidCounter === 1) {
-          data.all_dates.push(`Paid`);
-        } else if(data.paidCounter > 1){
-          data.all_dates.push(`Paid x${data.paidCounter}`);
-        }
-
-        if(data.giftCounter === 1){
-          data.all_dates.push(`Giftcards sent`);
-        } else if(data.giftCounter > 1){
-          data.all_dates.push(`${data.giftCounter} Giftcards Sent`)
-        }
-      
+         
         //Separera strängen med , tecknet. Annars om det inte finns många olika värden behåll det första
         if (data.all_dates.length > 1) {
           data.all_dates = data.all_dates.join(', ');  
@@ -151,7 +157,10 @@ export default async function mergeData() {
             existingRow.email !== data.email ||
             existingRow.address !== data.address ||
             existingRow.postcode !== data.postcode ||
-            existingRow.all_dates !== data.all_dates;
+            existingRow.all_dates !== data.all_dates ||
+            existingRow.giftcards_sent !== data.giftcards_sent ||
+            existingRow.latest_date !== data.latest_date ||
+            existingRow.modified !== data.modified;
 
           if (hasChanges) {
             await Poll.query(
@@ -162,7 +171,7 @@ export default async function mergeData() {
                 custom_field_1 = ?, custom_field_2 = ?, custom_field_3 = ?, 
                 custom_field_4 = ?, custom_field_5 = ?, affiliated_views_generated = ?, 
                 affiliated_leads_generated = ?, affiliated_money_generated = ?, 
-                tags = ?, all_dates = ?, latest_date = ? 
+                tags = ?, all_dates = ?, latest_date = ?, modified = ?
               WHERE phone = ?`,
               [
                 data.locations,
@@ -187,10 +196,11 @@ export default async function mergeData() {
                 data.tags,
                 data.all_dates,
                 data.latest_date,
+                data.modified,
                 data.phone,
               ]
             );
-            console.log(`Record updated for phone: ${data.phone}`);
+            // console.log(`Record updated for phone: ${data.phone}`);
           } else {
             console.log(`No changes for phone: ${data.phone}`);
           }
@@ -201,8 +211,8 @@ export default async function mergeData() {
              email, address, zip, city, personal_number, quiz_answers, custom_field_1, 
              custom_field_2, custom_field_3, custom_field_4, custom_field_5, 
              affiliated_views_generated, affiliated_leads_generated, 
-             affiliated_money_generated, tags, all_dates, latest_date) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+             affiliated_money_generated, tags, all_dates, latest_date, created, modified) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
             [
               data.locations,
               data.participants_id,
@@ -226,6 +236,8 @@ export default async function mergeData() {
               data.affiliated_money_generated,
               data.tags,
               data.all_dates,
+              data.created,
+              data.modified,
               data.latest_date,
             ]
           );
