98f65c82ccd8be5761ed4c70ffd339761b913a2b
Yonatan Zewude <yonatanzewude@hotmail.com>
2024-12-06 09:27:09 +0100

    Add initial cron job setup for daily data merging

--- src/jobs/dailyMergeJob.js
+++ src/jobs/dailyMergeJob.js
@@ -1,12 +1,27 @@
-import mergeData from "../services/mergeData.js"; // Standardimport
 import cron from "node-cron";
-
+import { dbConnection } from "../db_config/database.js";
+import mergeData from "../services/mergeData.js";
 
 export const dailyJob = async () => {
   cron.schedule("* * * * *", async () => {
     console.log("Startar sammanslagningsjobbet (varje minut)...");
     try {
-      await mergeData(); // Kör mergeData-funktionen
+      const connections = await dbConnection();
+
+      for (let i = 0; i < connections.length; i++) {
+        const Poll = connections[i].Poll;
+        const [participants] = await Poll.query(`
+          SELECT * FROM participant
+          WHERE created >= DATE_SUB(NOW(), INTERVAL 1 DAY)
+        `);
+
+        if (participants.length === 0) {
+          console.log("No participants found in the last 24 hours.");
+          continue;
+        }
+
+        await mergeData(participants);
+      }
 
       console.log("Sammanslagningsjobb slutfört.");
     } catch (error) {
@@ -14,5 +29,5 @@ export const dailyJob = async () => {
     }
   });
 
-  console.log("Cron-jobbet är startat och körs varje minut.");
+  console.log("Cron-jobbet är startat och körs varje minut."); // Loggar att cron-jobbet är startat.
 };
